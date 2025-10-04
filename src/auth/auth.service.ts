b8563c93843ad87c78323f538f8207cbd9bc9529
aayus karki <karki.aayush2003@icloud.com>
2025-10-04 15:02:23 +0545

    Auth module completed

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { RegisterDto } from './dto/registerUser.dto';
 import bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
 import { access } from 'fs';
+import { LoginDto } from './dto/loginUser.dto';
 
 @Injectable()
 export class AuthService {
@@ -22,7 +23,22 @@ export class AuthService {
         return { access_token: token };
     }
 
-    loginUser() {
-        return 'User logged in';
+    async loginUser(loginUserDto:LoginDto) {
+        const { email, password } = loginUserDto;
+        const user = await this.userService.findByEmail(email);
+
+        if (!user) {
+            throw new Error('Invalid credentials');
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            throw new Error('Invalid credentials');
+        }
+
+        const payload = { sub: user._id };
+        const token = await this.jwtService.signAsync(payload);
+        return { access_token: token };
     }
+
 }
